80ad14100570024b4d614d03137e177720624f8b
AndyCrook123 <40488095@live.napier.ac.uk>
2022-12-01 13:52:40 +0000

    project setup

--- fastfoodapp.py
+++ fastfoodapp.py
@@ -1,37 +1,42 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
     return render_template('homepage.html')
 
-@app.route("/favourites/")
+@app.route("/favourites.html")
 def favourites():
     return render_template('favourites.html')
 
-@app.route("/burgers/")
+@app.route("/burgers.html")
 def burgers():
     return render_template('burgers.html')
 
-@app.route("/italian/")
+@app.route("/italian.html")
 def italian():
     return render_template('italian.html')
 
-@app.route("/indian/")
+@app.route("/indian.html")
 def indian():
     return render_template('indian.html')
 
-@app.route("/chinese/")
+@app.route("/chinese.html")
 def chinese():
     return render_template('chinese.html')
 
-@app.route("/contactus/")
+@app.route("/contactus.html")
 def contactus():
     return render_template('contactus.html')
 
-@app.route("/login/")
+@app.route("/login.html")
 def login():
     return render_template('login.html')
+
+@app.route("/signup.html")
+def signup():
+    return render_template('signup.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")

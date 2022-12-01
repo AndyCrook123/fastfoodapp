a3977fb0aafd588c7a6c873e3d2559c05bb2996f
AndyCrook123 <40488095@live.napier.ac.uk>
2022-12-01 13:28:04 +0000

    Revert "Added sign up html page"
    This reverts commit f781515a7822f343fc9103e0ab67e116d96ad257.

--- fastfoodapp.py
+++ fastfoodapp.py
@@ -1,43 +1,37 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
     return render_template('homepage.html')
 
-@app.route("/favourites.html")
+@app.route("/favourites/")
 def favourites():
     return render_template('favourites.html')
 
-@app.route("/burgers.html")
+@app.route("/burgers/")
 def burgers():
     return render_template('burgers.html')
 
-@app.route("/italian.html")
+@app.route("/italian/")
 def italian():
     return render_template('italian.html')
 
-@app.route("/indian.html")
+@app.route("/indian/")
 def indian():
     return render_template('indian.html')
 
-@app.route("/chinese.html")
+@app.route("/chinese/")
 def chinese():
     return render_template('chinese.html')
 
-@app.route("/contactus.html")
+@app.route("/contactus/")
 def contactus():
     return render_template('contactus.html')
 
-@app.route("/login.html")
+@app.route("/login/")
 def login():
     return render_template('login.html')
-
-@app.route("/signup.html")
-def signup():
-    return render_template('signup.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")

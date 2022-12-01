8f168a6db6bd2bdb54e87f9d82af642c69021a72
AndyCrook123 <40488095@live.napier.ac.uk>
2022-12-01 15:01:25 +0000

    fixed routes

--- fastfoodapp.py
+++ fastfoodapp.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+  
 @app.route("/")
-def homepage():
+def root():
     return render_template('homepage.html')
 
 @app.route("/favourites.html")
